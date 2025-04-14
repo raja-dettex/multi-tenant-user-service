be73f8c823adf791df8c355be85bb6e468faeba4
Raja Khan <raja@localhost.localdomain>
2025-04-14 04:34:49 -0400

    update middleware cors

--- main.py
+++ main.py
@@ -3,7 +3,14 @@
 from ticket.server import app
 from ticket.models import TicketRepo, Ticket
 import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
 
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],       # not ["*"]!
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 uvicorn.run(app=app, host='0.0.0.0', port=8080)
